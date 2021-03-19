0538e7a9d2425024d666f71a2fe4b45266ae7f8b
thaseyor <56724794+thaseyor@users.noreply.github.com>
2021-03-19 20:42:45 +0600

    refresh token real fix

--- server/bin/refresh-tokens.js
+++ server/bin/refresh-tokens.js
@@ -9,49 +9,62 @@ mongoose.connect(
   () => console.log(`Database Successfully Connected`)
 );
 
-User.find({}, (err, users) => {
+function rewriteTokens(refresh_token, spotifyID, cb) {
+  let refreshOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    form: {
+      refresh_token,
+      grant_type: "refresh_token",
+    },
+    headers: {
+      Authorization:
+        "Basic " +
+        Buffer.from(
+          process.env.SPOTIFY_CLIENT_ID +
+            ":" +
+            process.env.SPOTIFY_CLIENT_SECRET
+        ).toString("base64"),
+    },
+    json: true,
+  };
+
+  request.post(refreshOptions, async function(error, response, body) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    const filter = { spotifyID };
+    const update = {
+      lastSpotifyToken: body.access_token,
+    };
+
+    await User.findOneAndUpdate(filter, update, {
+      new: true,
+      upsert: true,
+    });
+
+    console.log("New token: " + body.access_token);
+    cb();
+  });
+}
+
+User.find({}, { _id: 0, refreshToken: 1, spotifyID: 1 }, (err, usersRaw) => {
   if (err) {
     console.log(err);
     return;
   }
-  users.forEach((user) => {
+  let users = usersRaw.filter((user) => {
     if (user.refreshToken) {
-      let refresh_token = user.refreshToken;
-      let spotifyID = user.spotifyID;
-      let refreshOptions = {
-        url: "https://accounts.spotify.com/api/token",
-        form: {
-          refresh_token,
-          grant_type: "refresh_token",
-        },
-        headers: {
-          Authorization:
-            "Basic " +
-            Buffer.from(
-              process.env.SPOTIFY_CLIENT_ID +
-                ":" +
-                process.env.SPOTIFY_CLIENT_SECRET
-            ).toString("base64"),
-        },
-        json: true,
-      };
-      request.post(refreshOptions, async function(error, response, body) {
-        if (error) {
-          console.log(error);
-          return;
-        }
-        console.log("New token: " + body.access_token);
-        const filter = { spotifyID };
-        const update = {
-          lastSpotifyToken: body.access_token,
-        };
-
-        await User.findOneAndUpdate(filter, update, {
-          new: true,
-          upsert: true,
-        });
-        process.exit();
-      });
+      return user;
     }
   });
+
+  let requests = users.map((user) => {
+    return new Promise((resolve) => {
+      rewriteTokens(user.refreshToken, user.spotifyID, resolve);
+    });
+  });
+
+  Promise.all(requests).then(() => process.exit());
 });
