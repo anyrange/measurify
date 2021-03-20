56fade7f55fcef5860334da8acf9902354904589
thaseyor <56724794+thaseyor@users.noreply.github.com>
2021-03-20 17:27:20 +0600

    mongo history api projections

--- server/router.js
+++ server/router.js
@@ -17,8 +17,18 @@ router.get("/getAccessToken", auth.getAccessToken);
 router.get("/getPlayedHistory", (req, res) => {
   const spotifyID = req.query.spotifyID;
   const projection = {
-    recentlyPlayed: 1,
-  };
+    _id: 0,
+
+    "recentlyPlayed.track.album.id": 1,
+    "recentlyPlayed.track.album.name": 1,
+    "recentlyPlayed.track.artists.id": 1,
+    "recentlyPlayed.track.artists.name": 1,
+    "recentlyPlayed.track.duration_ms": 1,  
+    "recentlyPlayed.track.id": 1,
+    "recentlyPlayed.track.name": 1,
+    "recentlyPlayed.played_at": 1,
+  }
+  
   User.findOne({ spotifyID }, projection, (err, user) => {
     if (err) {
       console.log(err);
