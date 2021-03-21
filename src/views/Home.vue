7f3ec107fe25d47344fc3d970592276fdfdd1e93
wsehl <widesehl@gmail.com>
2021-03-22 00:08:41 +0600

    remove search from home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,30 +4,6 @@
       <div class="flex-1 flex overflow-y-hidden">
         <DashboardNav />
         <div class="bg-gray-800-spotify flex-1 flex flex-col">
-          <div class="top-bar flex items-center justify-between px-4 py-2">
-            <div class="flex items-center">
-              <div class="ml-4 mt-2 relative">
-                <input
-                  type="text"
-                  placeholder="Search"
-                  class="bg-white text-gray-800 placeholder-gray-800 rounded-full px-3 pl-8 py-1"
-                />
-                <div class="absolute top-0">
-                  <svg
-                    class="fill-current text-gray-800 h-6 w-6 pt-1 pl-2"
-                    viewBox="0 0 24 24"
-                    width="24"
-                    height="24"
-                  >
-                    <path
-                      class="heroicon-ui"
-                      d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </div>
           <div class="content-spotify overflow-y-auto">
             <router-view />
           </div>
