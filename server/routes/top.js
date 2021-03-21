a7758010b3359a88fde695786ef22c38b7469355
thaseyor <56724794+thaseyor@users.noreply.github.com>
2021-03-21 21:51:19 +0600

    top: tracks

--- server/routes/top.js
+++ server/routes/top.js
@@ -4,10 +4,12 @@ const albums = (recentlyPlayed) => {
   let albums = [];
 
   while (recentlyPlayed.length) {
+    // get array of tracks from one album
     let filteredHistory = recentlyPlayed.filter(
       (item) => item.track.album.name === recentlyPlayed[0].track.album.name
     );
 
+    // calculate duration
     let reducer = (accumulator, currentValue) =>
       accumulator + currentValue.track.duration_ms;
 
@@ -27,6 +29,45 @@ const albums = (recentlyPlayed) => {
 
   // sort by time
   return albums.sort(function(a, b) {
+    if (a.playtime < b.playtime) {
+      return 1;
+    }
+    if (a.playtime > b.playtime) {
+      return -1;
+    }
+    return 0;
+  });
+};
+
+const tracks = (recentlyPlayed) => {
+  let tracks = [];
+
+  while (recentlyPlayed.length) {
+    // get array of simillar tracks
+    let filteredHistory = recentlyPlayed.filter(
+      (item) => item.track.name === recentlyPlayed[0].track.name
+    );
+
+    // calculate duration
+    let reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.track.duration_ms;
+
+    playtime = Math.round(filteredHistory.reduce(reducer, 0) / 1000 / 60);
+
+    tracks.push({
+      name: recentlyPlayed[0].track.name,
+      image: recentlyPlayed[0].track.album.images[2].url,
+      id: recentlyPlayed[0].track.id,
+      playtime,
+    });
+
+    recentlyPlayed = recentlyPlayed.filter(
+      (item) => item.track.name !== recentlyPlayed[0].track.name
+    );
+  }
+
+  // sort by time
+  return tracks.sort(function(a, b) {
     if (a.playtime < b.playtime) {
       return 1;
     }
@@ -59,6 +100,7 @@ const top = (req, res) => {
     }
     let response = {};
     response.albums = albums(user.recentlyPlayed);
+    response.tracks = tracks(user.recentlyPlayed);
 
     res.end(JSON.stringify(response));
   });
