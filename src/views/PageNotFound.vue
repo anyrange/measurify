94a809ccbe8ffc02f8161f727e7032f3a576e805
wsehl <widesehl@gmail.com>
2021-03-23 03:32:44 +0600

    Update PageNotFound.vue

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,14 +1,25 @@
 <template>
-  <div class="container mx-auto">
-    <div class="mt-12">
-      <router-link
-        class="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium mx-auto"
-        to="/"
-      >
-        <span class="text-gray-600">
-          Go home
-        </span>
-      </router-link>
+  <div class="h-screen w-screen bg-gray-800-spotify flex items-center">
+    <div
+      class="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-300"
+    >
+      <div class="max-w-md">
+        <div class="text-5xl font-dark font-bold mb-2">404</div>
+        <p class="text-2xl md:text-3xl font-light leading-normal mb-4">
+          Sorry we couldn't find this page.
+        </p>
+        <p class="mb-8">
+          But dont worry, you can find plenty of other things on our homepage.
+        </p>
+        <router-link to="/" c>
+          <button
+            class="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-green-600-spotify hover:bg-green-700-spotify"
+          >
+            Back to home
+          </button>
+        </router-link>
+      </div>
+      <div class="max-w-lg"></div>
     </div>
   </div>
 </template>
