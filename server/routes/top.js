8865eaef9baf427e66f8a2d848929abec90be9ae
thaseyor <56724794+thaseyor@users.noreply.github.com>
2021-03-22 11:43:32 +0600

    top: artists

--- server/routes/top.js
+++ server/routes/top.js
@@ -1,82 +1,5 @@
 const User = require("../models/User");
-
-const albums = (recentlyPlayed) => {
-  let albums = [];
-
-  while (recentlyPlayed.length) {
-    // get array of tracks from one album
-    let filteredHistory = recentlyPlayed.filter(
-      (item) => item.track.album.name === recentlyPlayed[0].track.album.name
-    );
-
-    // calculate duration
-    let reducer = (accumulator, currentValue) =>
-      accumulator + currentValue.track.duration_ms;
-
-    playtime = Math.round(filteredHistory.reduce(reducer, 0) / 1000 / 60);
-
-    albums.push({
-      name: recentlyPlayed[0].track.album.name,
-      image: recentlyPlayed[0].track.album.images[2].url,
-      id: recentlyPlayed[0].track.album.id,
-      playtime,
-    });
-
-    recentlyPlayed = recentlyPlayed.filter(
-      (item) => item.track.album.name !== recentlyPlayed[0].track.album.name
-    );
-  }
-
-  // sort by time
-  return albums.sort(function(a, b) {
-    if (a.playtime < b.playtime) {
-      return 1;
-    }
-    if (a.playtime > b.playtime) {
-      return -1;
-    }
-    return 0;
-  });
-};
-
-const tracks = (recentlyPlayed) => {
-  let tracks = [];
-
-  while (recentlyPlayed.length) {
-    // get array of simillar tracks
-    let filteredHistory = recentlyPlayed.filter(
-      (item) => item.track.name === recentlyPlayed[0].track.name
-    );
-
-    // calculate duration
-    let reducer = (accumulator, currentValue) =>
-      accumulator + currentValue.track.duration_ms;
-
-    playtime = Math.round(filteredHistory.reduce(reducer, 0) / 1000 / 60);
-
-    tracks.push({
-      name: recentlyPlayed[0].track.name,
-      image: recentlyPlayed[0].track.album.images[2].url,
-      id: recentlyPlayed[0].track.id,
-      playtime,
-    });
-
-    recentlyPlayed = recentlyPlayed.filter(
-      (item) => item.track.name !== recentlyPlayed[0].track.name
-    );
-  }
-
-  // sort by time
-  return tracks.sort(function(a, b) {
-    if (a.playtime < b.playtime) {
-      return 1;
-    }
-    if (a.playtime > b.playtime) {
-      return -1;
-    }
-    return 0;
-  });
-};
+const request = require("request");
 
 const top = (req, res) => {
   const spotifyID = req.query.spotifyID;
@@ -91,9 +14,10 @@ const top = (req, res) => {
     "recentlyPlayed.track.duration_ms": 1,
     "recentlyPlayed.track.id": 1,
     "recentlyPlayed.track.name": 1,
+    lastSpotifyToken: 1,
   };
 
-  User.findOne({ spotifyID }, projection, (err, user) => {
+  User.findOne({ spotifyID }, projection, async (err, user) => {
     if (err) {
       console.log(err);
       return;
@@ -101,9 +25,169 @@ const top = (req, res) => {
     let response = {};
     response.albums = albums(user.recentlyPlayed);
     response.tracks = tracks(user.recentlyPlayed);
+    response.artists = await artists(user.recentlyPlayed);
 
-    res.end(JSON.stringify(response));
+    // get images for artists
+    const artistsOptions = {
+      uri:
+        "https://api.spotify.com/v1/artists?ids=" +
+        response.artists.map((artist) => artist.id).join(),
+      headers: {
+        Authorization: "Bearer " + user.lastSpotifyToken,
+      },
+      json: true,
+    };
+
+    request.get(artistsOptions, async (error, resp, body) => {
+      await response.artists.forEach((artist, index) => {
+        artist.image = body.artists[index].images[2].url;
+      });
+      res.end(JSON.stringify(response));
+    });
   });
+};
+
+const albums = (recentlyPlayed) => {
+  let albums = [];
+
+  while (recentlyPlayed.length) {
+    // get array of tracks from one album
+    const filteredHistory = recentlyPlayed.filter(
+      (item) => item.track.album.name === recentlyPlayed[0].track.album.name
+    );
+
+    // calculate duration
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.track.duration_ms;
+
+    const playtime = Math.round(filteredHistory.reduce(reducer, 0) / 1000 / 60);
+
+    albums.push({
+      name: recentlyPlayed[0].track.album.name,
+      image: recentlyPlayed[0].track.album.images[2].url,
+      id: recentlyPlayed[0].track.album.id,
+      playtime,
+    });
+
+    recentlyPlayed = recentlyPlayed.filter(
+      (item) => item.track.album.name !== recentlyPlayed[0].track.album.name
+    );
+  }
+
+  // sort by time and get first 20 elements
+  return albums
+    .sort(function(a, b) {
+      if (a.playtime < b.playtime) {
+        return 1;
+      }
+      if (a.playtime > b.playtime) {
+        return -1;
+      }
+      return 0;
+    })
+    .slice(0, 20);
+};
+
+const tracks = (recentlyPlayed) => {
+  let tracks = [];
+
+  while (recentlyPlayed.length) {
+    // get array of simillar tracks
+    const filteredHistory = recentlyPlayed.filter(
+      (item) => item.track.name === recentlyPlayed[0].track.name
+    );
+
+    // calculate duration
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.track.duration_ms;
+
+    const playtime = Math.round(filteredHistory.reduce(reducer, 0) / 1000 / 60);
+
+    tracks.push({
+      name: recentlyPlayed[0].track.name,
+      image: recentlyPlayed[0].track.album.images[2].url,
+      id: recentlyPlayed[0].track.id,
+      playtime,
+    });
+
+    recentlyPlayed = recentlyPlayed.filter(
+      (item) => item.track.name !== recentlyPlayed[0].track.name
+    );
+  }
+
+  // sort by time and get first 20 elements
+  return tracks
+    .sort(function(a, b) {
+      if (a.playtime < b.playtime) {
+        return 1;
+      }
+      if (a.playtime > b.playtime) {
+        return -1;
+      }
+      return 0;
+    })
+    .slice(0, 20);
+};
+
+const artists = (recentlyPlayed, lastSpotifyToken) => {
+  let artists = [];
+  let uniqueArtistsNames = [];
+  let uniqueArtists = [];
+
+  recentlyPlayed.forEach(({ track }) =>
+    track.artists.forEach((artist) => {
+      if (!uniqueArtistsNames.includes(artist.name)) {
+        uniqueArtistsNames.push(artist.name);
+        uniqueArtists.push({ name: artist.name, id: artist.id });
+      }
+    })
+  );
+
+  while (uniqueArtists.length) {
+    let playtime = 0;
+
+    // checks if artist is array of artists
+    for (let i = 0; i < 3; i++) {
+      const filteredHistory = recentlyPlayed.filter((item) => {
+        if (
+          item.track.artists.length - 1 >= i &&
+          item.track.artists[i].name === uniqueArtists[0].name
+        ) {
+          return item;
+        }
+      });
+
+      // calculate duration
+      const reducer = (accumulator, currentValue) =>
+        accumulator + currentValue.track.duration_ms;
+      if (filteredHistory.length) {
+        playtime += Math.round(filteredHistory.reduce(reducer, 0) / 1000 / 60);
+      }
+    }
+
+    artists.push({
+      name: uniqueArtists[0].name,
+      id: uniqueArtists[0].id,
+      playtime,
+    });
+
+    uniqueArtists.shift();
+  }
+
+  // sort by time and get first 20 elements
+  let sortedArtists = artists
+    .sort(function(a, b) {
+      if (a.playtime < b.playtime) {
+        return 1;
+      }
+      if (a.playtime > b.playtime) {
+        return -1;
+      }
+      return 0;
+    })
+    .slice(0, 20);
+
+  return sortedArtists;
 };
 
 module.exports = top;
