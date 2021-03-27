749050c8efba988274457591e3619adcd0c97b18
thaseyor <56724794+thaseyor@users.noreply.github.com>
2021-03-27 14:12:44 +0600

    x10 recently played refresh optimization

--- server/includes/recently-played-parse.js
+++ server/includes/recently-played-parse.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 function refresh_recently_played() {
   const start = new Date();
-  function parseRecentlyPlayed(user, cb) {
+  function parseRecentlyPlayed(user, cb, reject) {
     const recentlyPlayedOptions = {
       uri: "https://api.spotify.com/v1/me/player/recently-played?limit=15",
       headers: {
@@ -13,13 +13,9 @@ function refresh_recently_played() {
       json: true,
     };
 
-    // delete smth from all users
-    // user.recentlyPlayed.forEach((item)=>delete item.track.available_markets);
-
     request.get(recentlyPlayedOptions, async (error, response, body) => {
-      if (error) {
-        consol.log(error);
-        return;
+      if (error || !body.items) {
+        reject(user.userName, error, body);
       }
       body.items.forEach((item) => delete item.track.available_markets);
 
@@ -41,11 +37,17 @@ function refresh_recently_played() {
           newSongs.push(body.items[i]);
           i++;
         }
-        user.recentlyPlayed.unshift(...newSongs);
+
         const query = { spotifyID: user.spotifyID };
         const update = {
-          recentlyPlayed: user.recentlyPlayed,
+          $push: {
+            recentlyPlayed: {
+              $each: newSongs,
+              $position: 0,
+            },
+          },
         };
+
         await User.updateOne(query, update);
         cb();
       }
@@ -54,24 +56,34 @@ function refresh_recently_played() {
 
   User.find(
     {},
-    { _id: 0, spotifyID: 1, lastSpotifyToken: 1, recentlyPlayed: 1 },
+    {
+      _id: 0,
+      spotifyID: 1,
+      lastSpotifyToken: 1,
+      recentlyPlayed: { $slice: [0,1] },
+      userName: 1,
+    },
     (err, users) => {
       if (err) {
         console.log(err);
         return;
       }
-
+      
       // ## ONE TIME PROMISE (TASKS MAY BE EXECUTED IN RANDOM ORDER)
 
       let requests = users.map((user) => {
-        return new Promise((resolve) => {
-          parseRecentlyPlayed(user, resolve);
+        return new Promise((resolve, reject) => {
+          parseRecentlyPlayed(user, resolve, reject);
+        }).catch((user) => {
+          console.log(user + " died");
+          console.log("error: " + error);
+          console.log("body: " + body);
         });
       });
 
       Promise.all(requests).then(() => {
         console.log(
-          `All ${users.length} histories updated at ` +
+          `All ${requests.length} histories updated at ` +
             new Date().toLocaleString("en-US", { timeZone: "Asia/Almaty" })
         );
         const end = new Date();
