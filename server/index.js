2d378275854c1725ba9fa1ab2f42f5c626d6fb09
thaseyor <56724794+thaseyor@users.noreply.github.com>
2021-03-27 18:45:50 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,14 +7,14 @@ const refresh_recently_played = require("./includes/recently-played-parse.js");
 
 const app = express();
 const PORT = process.env.PORT || 8888;
-
+const FRONTEND_URI = process.env.FRONTEND_URI;
 app.listen(PORT, () => {
   console.log(`App listening on port: ${PORT}`);
 });
 
 if (process.env.NODE_ENV == "production") {
   app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URI);
+    res.header("Access-Control-Allow-Origin", FRONTEND_URI);
     next();
   });
 }
