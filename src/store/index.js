b4eea63d650f341d829c8973425594feabfb3acf
wsehl <widesehl@gmail.com>
2021-04-25 22:12:40 +0600

    secure localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,21 @@
 import { createStore } from "vuex";
 import createPersistedState from "vuex-persistedstate";
 import api from "@/api";
+import SecureLS from "secure-ls";
+const ls = new SecureLS({ isCompression: false });
 
 export default createStore({
   state: {
     user: null,
+    id: null,
     access_token: null,
   },
   mutations: {
     SET_USER(state, payload) {
       state.user = payload;
+    },
+    SET_ID(state, payload) {
+      state.id = payload;
     },
     SET_ACCES_TOKEN(state, payload) {
       state.access_token = payload;
@@ -20,7 +26,7 @@ export default createStore({
       return state.user;
     },
     getUserID(state) {
-      return state.user._id;
+      return state.id;
     },
     getAccessToken(state) {
       return state.access_token;
@@ -30,16 +36,29 @@ export default createStore({
     login: () => {
       api.login();
     },
-    logout: ({ commit }) => {
-      commit("SET_USER", null);
-      commit("SET_ACCES_TOKEN", null);
+    logout: () => {
+      window.localStorage.clear();
+      window.sessionStorage.clear();
+      // let script = document.createElement("script");
+      // script.src = "https://www.spotify.com/logout/";
+      // script.type = "text/javascript";
+      // document.getElementById("app").appendChild(script);
+      location.reload();
     },
     authorise: async ({ commit }, { access_token, id }) => {
       const response = await api.authorise(access_token);
-      response._id = id;
+      commit("SET_ID", id);
       commit("SET_USER", response);
       commit("SET_ACCES_TOKEN", access_token);
     },
   },
-  plugins: [createPersistedState()],
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: (key) => ls.get(key),
+        setItem: (key, value) => ls.set(key, value),
+        removeItem: (key) => ls.remove(key),
+      },
+    }),
+  ],
 });
