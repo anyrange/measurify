59f225e5c591c574aff187cd5cd1c0107bf91a48
thaseyor <56724794+thaseyor@users.noreply.github.com>
2021-04-29 11:30:03 +0600

    Update authMiddleware.js

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,6 +1,6 @@
 const authMiddleware = (req, res, next) => {
   const _id = req.get("Authorization");
-  console.log(typeof _id);
+  console.log(_id.split("2")[0]);
   if (!_id) {
     res.status(401).json({ message: `Unauthorized` });
     return;
