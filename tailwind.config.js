18bfe336ecfb6159f7dfcbffbf983ef00c2edd20
wsehl <widesehl@gmail.com>
2021-05-17 21:38:31 +0600

    new tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
+  purge: ["./public/**/*.html", "./src/**/*.vue"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
