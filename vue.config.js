aeb91cb2aa379a19f0095daa5b1fd13976345d9e
wsehl <widesehl@gmail.com>
2021-05-20 21:57:28 +0600

    new favicon

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,25 @@
 module.exports = {
   pwa: {
-    name: "Spotiworm",
+    manifestOptions: {
+      name: "Spotiworm",
+      short_name: "Spotiworm",
+      display: "standalone",
+      theme_color: "#000000",
+    },
+    themeColor: "#414141",
+    msTileColor: "#414141",
+    icons: [
+      {
+        src: "./img/icons/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "./img/icons/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
     workboxPluginMode: "InjectManifest",
     workboxOptions: {
       swSrc: "./src/sw.js",
