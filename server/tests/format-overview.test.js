859df72796e0df58fd6d59f4e2dc3beaf10c9df3
thaseyor <56724794+thaseyor@users.noreply.github.com>
2021-05-31 22:45:11 +0600

    test: remove useless tests

--- server/tests/format-overview.test.js
+++
@@ -1,9 +0,0 @@
-import overview from "../includes/format-overview.js";
-
-test("overview user with empty array", () => {
-  expect(overview([])).toEqual([]);
-});
-
-test("overview user with empty data", () => {
-  expect(overview()).toEqual([]);
-});
