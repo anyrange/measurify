dc7c6e9d5bde2582a89b10fe3a325f77a63e5918
thaseyor <56724794+thaseyor@users.noreply.github.com>
2021-06-18 12:39:34 +0600

    fix: logout without validation

--- server/routes/logout/index.js
+++ server/routes/logout/index.js
@@ -1,15 +1,6 @@
 export default async function(fastify) {
-  const headers = fastify.getSchema("cookie");
-  fastify.get(
-    "",
-    {
-      schema: {
-        headers,
-      },
-    },
-    (request, reply) => {
-      reply.clearCookie("token", { path: "/" });
-      reply.send({ message: "OK", status: 200 });
-    }
-  );
+  fastify.get("", (request, reply) => {
+    reply.clearCookie("token", { path: "/" });
+    reply.send({ message: "OK", status: 200 });
+  });
 }
