edf70a71ec4a95dbc455de205b84787d603248a3
thaseyor <56724794+thaseyor@users.noreply.github.com>
2021-06-19 23:57:20 +0600

    revert: turn off smart-playlist

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 import cron from "node-cron";
 import refresh_tokens from "./includes/refresh-tokens.js";
 import refresh_recently_played from "./includes/recently-played-parse.js";
-import * as smartPlaylist from "./includes/smart-playlist.js";
+// import * as smartPlaylist from "./includes/smart-playlist.js";
 import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 8888;
@@ -44,10 +44,10 @@ function startScheduledJobs() {
     refresh_recently_played();
   });
 
-  cron.schedule("0 0 * * *", () => {
-    smartPlaylist.update();
-  });
-  cron.schedule("*/15 * * * *", () => {
-    smartPlaylist.clean();
-  });
+  // cron.schedule("0 0 * * *", () => {
+  //   smartPlaylist.update();
+  // });
+  // cron.schedule("*/15 * * * *", () => {
+  //   smartPlaylist.clean();
+  // });
 }
