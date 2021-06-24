66034e3e17d29e27d0b908db946c6a2ff2463de7
Alex Sehl <widesehl@gmail.com>
2021-06-24 21:00:27 +0600

    fix: disable routing to /overview for logged-in users

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,24 @@
 <script>
 import Notifications from "@/components/Notifications";
 import UpdateNotification from "@/components/UpdateNotification";
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   components: {
     UpdateNotification,
     Notifications,
   },
+  computed: {
+    ...mapGetters(["isAuthenticated"]),
+  },
   methods: {
     ...mapActions(["updateUser", "logout"]),
   },
   async created() {
+    const isAuthenticated = this.isAuthenticated;
     try {
       await this.updateUser();
-      this.$router.push({ name: "home" });
+      if (!isAuthenticated) this.$router.push({ name: "home" });
     } catch {
       this.logout();
     }
