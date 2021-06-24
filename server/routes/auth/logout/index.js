5becf6c8fde66be542e506e130f1e75800be05e6
thaseyor <56724794+thaseyor@users.noreply.github.com>
2021-06-24 19:02:05 +0600

    fix: logout

--- server/routes/auth/logout/index.js
+++ server/routes/auth/logout/index.js
@@ -1,6 +1,10 @@
 export default async function(fastify) {
   fastify.get("", (request, reply) => {
-    reply.clearCookie("token", { path: "/" });
+    reply.clearCookie("token", {
+      path: "/",
+      secure: true,
+      sameSite: "none",
+    });
     reply.send({ message: "OK", status: 200 });
   });
 }
