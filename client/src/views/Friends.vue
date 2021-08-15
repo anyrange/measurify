35e8ac206d3ae99b4220e1b223943e52ec679108
Alex Sehl <widesehl@gmail.com>
2021-08-15 22:14:07 +0600

    feat: sort friends by last login date

--- client/src/views/Friends.vue
+++ client/src/views/Friends.vue
@@ -1,38 +1,44 @@
 <template>
   <h1 class="h-title">Friends</h1>
   <loading-spinner v-if="loading" />
-  <div v-else class="flex flex-col gap-3 lg:w-1/2 xl:w-1/4">
-    <div
-      v-for="item in friends"
+  <div v-else class="flex flex-col gap-4 sm:gap-3 lg:w-1/2 xl:w-1/4">
+    <router-link
+      class="
+        hover:bg-gray-700-spotify
+        sm:p-2
+        p-0
+        sm:rounded-lg
+        rounded-full
+        cursor-pointer
+      "
+      v-for="item in friendsSortedByLastLogin"
       :key="item.customID"
-      class="hover:bg-gray-700-spotify p-2 rounded-lg cursor-pointer"
+      :to="{ name: 'profile', params: { id: item.customID } }"
     >
-      <router-link :to="{ name: 'profile', params: { id: item.customID } }">
-        <div class="flex flex-row gap-4 items-center">
-          <base-img
-            :src="item.avatar"
-            :alt="item.customID"
-            avatar
-            class="w-16 h-16 object-cover rounded-full flex-shrink-0"
-          />
-          <div class="flex flex-col">
-            <div class="text-white text-base font-medium">
-              {{ item.userName }}
-            </div>
-            <div class="text-gray-500-spotify text-sm font-normal">
-              last seen {{ getDateFromNow(item.lastLogin) }}
-            </div>
+      <div class="flex flex-row gap-4 items-center">
+        <base-img
+          :src="item.avatar"
+          :alt="item.customID"
+          avatar
+          class="w-16 h-16 object-cover rounded-full flex-shrink-0"
+        />
+        <div class="flex flex-col">
+          <div class="text-white text-base font-medium">
+            {{ item.userName }}
+          </div>
+          <div class="text-gray-500-spotify text-sm font-normal">
+            last seen {{ getDateFromNow(item.lastLogin) }}
           </div>
         </div>
-      </router-link>
-    </div>
+      </div>
+    </router-link>
   </div>
 </template>
 
 <script>
 import { getFriends } from "@/api";
-import BaseImg from "@/components/BaseImg.vue";
 import { getDateFromNow } from "@/utils/formatters";
+import BaseImg from "@/components/BaseImg.vue";
 
 export default {
   name: "Friends",
@@ -43,14 +49,23 @@ export default {
       friends: [],
     };
   },
-  created() {
-    getFriends().then((response) => {
-      this.friends = response.friends;
-      this.loading = false;
-    });
+  computed: {
+    friendsSortedByLastLogin() {
+      return this.orderByDate(this.friends, "lastLogin").reverse();
+    },
+  },
+  async created() {
+    const { friends } = await getFriends();
+    this.friends = friends;
+    this.loading = false;
   },
   methods: {
     getDateFromNow,
+    orderByDate(array, key) {
+      return array.sort(
+        (a, b) => new Date(a[key]).getTime() - new Date(b[key]).getTime()
+      );
+    },
   },
 };
 </script>
