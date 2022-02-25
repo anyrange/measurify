90a5c2086196eb515fc4487369b92524cd8f352f
thaseyor <mikeston@mail.ru>
2022-02-25 13:09:53 +0600

    feat: listeners top

--- server/routes/users/top/index.js
+++ server/routes/users/top/index.js
@@ -5,21 +5,19 @@ export default async function (fastify) {
       schema: {
         response: {
           200: {
-            type: "object",
-            required: ["status", "top"],
-            properties: {
-              status: { type: "number" },
-              top: {
-                type: "array",
-                items: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                username: { type: "string" },
+                avatar: { type: "string" },
+                display_name: { type: "string" },
+                lastLogin: { type: "string", format: "datetime" },
+                listened: {
                   type: "object",
                   properties: {
-                    username: { type: "string" },
-                    avatar: { type: "string" },
-                    display_name: { type: "string" },
-                    canSee: { type: "boolean" },
-                    lastLogin: { type: "string", format: "datetime" },
-                    listened: { type: "number" },
+                    count: { type: "number" },
+                    time: { type: "number" },
                   },
                 },
               },
@@ -28,99 +26,29 @@ export default async function (fastify) {
         },
         tags: ["users"],
       },
-      preValidation: [fastify.auth],
     },
     async function (req, reply) {
-      const id = req.session.get("id");
-
-      const [requestor, top] = await Promise.all([
-        fastify.db.User.findById(id, "tokens.token"),
-        fastify.db.User.aggregate()
-          .match({
-            "tokens.refreshToken": { $ne: "" },
-            "settings.privacy": { $ne: "private" },
-            listeningHistory: { $ne: [] },
-          })
-          .project({
-            display_name: 1,
-            avatar: 1,
-            username: "$settings.username",
-            token: "$tokens.token",
-            lastLogin: 1,
-            privacy: "$settings.privacy",
-            listeningHistory: 1,
-          })
-          .unwind("$listeningHistory")
-          .lookup({
-            from: "tracks",
-            localField: "listeningHistory.track",
-            foreignField: "_id",
-            as: "listeningHistory",
-          })
-          .group({
-            _id: {
-              _id: "$_id",
-              username: "$username",
-              display_name: "$display_name",
-              avatar: "$avatar",
-              token: "$token",
-              privacy: "$privacy",
-              lastLogin: "$lastLogin",
+      const top = await fastify.db.User.find(
+        {
+          "tokens.refreshToken": { $ne: "" },
+          "settings.privacy": { $ne: "private" },
+          listeningHistory: { $ne: [] },
+        },
+        {
+          "listened.count": 1,
+          "listened.time": {
+            $round: {
+              $divide: ["$listened.time", 60],
             },
-            plays: { $sum: 1 },
-            listened: {
-              $sum: { $first: "$listeningHistory.duration_ms" },
-            },
-          })
-          .project({
-            _id: "$_id._id",
-            display_name: "$_id.display_name",
-            avatar: "$_id.avatar",
-            username: "$_id.username",
-            token: "$_id.token",
-            lastLogin: "$_id.lastLogin",
-            privacy: "$_id.privacy",
-            listened: { $round: { $divide: ["$listened", 60000] } },
-          })
-          .sort("-listened"),
-      ]);
+          },
+          display_name: 1,
+          avatar: 1,
+          lastLogin: 1,
+          username: "$settings.username",
+        }
+      ).lean();
 
-      const friendsOnly = top.filter(
-        (user) => user.privacy === "friendsOnly" && user._id !== requestor._id
-      );
-
-      if (friendsOnly.length) {
-        const [followedList, ...mutualFollowedList] = await Promise.all([
-          fastify.spotifyAPI({
-            route: `me/following/contains?type=user&ids=${friendsOnly
-              .map(({ _id }) => _id)
-              .join()}`,
-            token: requestor.tokens.token,
-          }),
-          ...friendsOnly.map((user) =>
-            fastify.spotifyAPI({
-              route: `me/following/contains?type=user&ids=${requestor._id}`,
-              token: user.token,
-            })
-          ),
-        ]);
-
-        const notFriends = friendsOnly.filter(
-          (user, key) => !mutualFollowedList[key][0] || !followedList[key]
-        );
-
-        top.forEach((user) => {
-          user.canSee = !notFriends.find(
-            (notFriend) => user._id === notFriend._id
-          );
-        });
-      } else {
-        top.forEach((user) => {
-          user.canSee = true;
-        });
-      }
-
-      reply.send({ top });
+      reply.send(top);
     }
   );
 }
