d1c9064ca2a38cbe90647037d447fc38f1bdf06c
Alex Sehl <widesehl@gmail.com>
2022-02-27 17:46:11 +0600

    fix: eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    "vue/setup-compiler-macros": true,
+  },
+  extends: [
+    "plugin:vue/vue3-strongly-recommended",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier",
+  ],
+  rules: {
+    "vue/multi-word-component-names": "off",
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+  },
+};
