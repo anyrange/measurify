464692e95d1f8784d6d5d5a3ea67a6991652a49a
Alex Sehl <widesehl@gmail.com>
2022-02-27 17:58:01 +0600

    refactor: layout

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <router-view />
-  <notifications />
+  <div class="h-screen">
+    <template v-if="loading">
+      <loading-spinner />
+    </template>
+    <template v-else>
+      <div
+        class="flex flex-col h-screen text-sm text-gray-500-spotify"
+        :class="{ 'custom-scrollbar': !isMobile }"
+      >
+        <div class="flex-1 flex overflow-y-hidden">
+          <app-navigation-sidebar />
+          <main class="flex-1 flex flex-col bg-gray-900-spotify">
+            <div
+              id="content-window"
+              class="
+                overflow-y-auto
+                flex flex-col
+                h-full
+                p-4
+                mb-12
+                sm:px-8 sm:mb-0
+              "
+            >
+              <router-view />
+            </div>
+          </main>
+          <app-friends-sidebar v-if="xlAndLarger" />
+        </div>
+      </div>
+    </template>
+    <notifications />
+  </div>
 </template>
 
 <script setup>
 import { provide } from "vue";
-import { useRouter } from "vue-router";
+import { useNavigator } from "@/composable/useNavigator";
+import { createAsyncProcess } from "@/composable/useAsync";
 import { useUserStore } from "@/stores/user";
+import { useBreakpoints } from "@vueuse/core";
+import { BREAKPOINTS } from "@/config";
 
-const router = useRouter();
-const user = useUserStore();
+const userStore = useUserStore();
+const { isMobile } = useNavigator();
+const breakpoints = useBreakpoints(BREAKPOINTS);
+
+const xlAndLarger = breakpoints.greater("xl");
 
 provide("historyLength", window.history.length);
 
-const isAuthenticated = user.isAuthenticated;
+const { loading, run } = createAsyncProcess(userStore.updateUser);
 
-(async () => {
-  try {
-    await user.updateUser();
-    !isAuthenticated && router.push({ name: "home" });
-  } catch {
-    await user.logout();
-  }
-})();
+run();
 </script>
