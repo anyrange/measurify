66dbeefc4c996d4797d60291ed9d6f43bc27785e
Alex Sehl <widesehl@gmail.com>
2022-03-05 23:28:03 +0600

    chore: remove unused apexcharts

--- client/src/main.js
+++ client/src/main.js
@@ -4,7 +4,6 @@ import { createPersistedState } from "./piniaPersisted.js";
 import App from "./App.vue";
 import router from "./router";
 import VWave from "v-wave";
-import ApexCharts from "apexcharts";
 import "virtual:windi.css";
 import "./assets/styles/index.css";
 import { registerSW } from "virtual:pwa-register";
@@ -12,7 +11,6 @@ import { registerSW } from "virtual:pwa-register";
 registerSW({ immediate: true });
 
 const app = createApp(App);
-app.config.globalProperties.$apexcharts = ApexCharts;
 
 const pinia = createPinia();
 pinia.use(createPersistedState({ modules: ["user", "settings"] }));
